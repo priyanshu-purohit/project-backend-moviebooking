83487cb76b207fc9a29bebb84146781475001e0a
priyanshu-purohit <purohitpriyanshu20@gmail.com>
2026-01-24 19:55:41 +0530

    Feat: handled validation failure for properties in theatre schema

--- controller/theatre.controller.js
+++ controller/theatre.controller.js
@@ -4,6 +4,11 @@ const { successResponseBody, errorResponseBody } = require('../utils/responsebod
 const create = async (req, res) => {
     try{
         const response = await theatreServices.createTheatre(req.body);
+        if(response.err){
+            errorResponseBody.err = response.err;
+            errorResponseBody.message = "Validation failed on few paramters of the request body";
+            return res.status(response.code).json(errorResponseBody);
+        }
         successResponseBody.message = 'Successfully created the theatre';
         successResponseBody.data = response;
         return res.status(201).json(successResponseBody);
