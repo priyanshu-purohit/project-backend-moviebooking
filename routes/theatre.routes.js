6e953768c0fad2510f80d1250f4651457c1769fe
priyanshu-purohit <purohitpriyanshu20@gmail.com>
2026-01-24 20:20:43 +0530

    Feat: Added get theatre api

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -3,6 +3,8 @@ const theatreMiddleware = require('../middlewares/theatre.middlewares');
 
 const routes = (app) => {
     app.post('/mba/api/v1/theatre', theatreMiddleware.validateTheatreCreateRequest, theatreController.create);
+
+    app.get('/mba/api/v1/theatre/:id', theatreController.getTheatre);
 }
 
 module.exports = routes;
--- services/theatre.services.js
+++ services/theatre.services.js
@@ -19,6 +19,24 @@ const createTheatre = async (data) => {
     }  
 }
 
+const getTheatre = async (id) => {
+    try{
+        const response = await Theatre.findById(id);
+        if(!response){
+            return {
+                err: "No theatre found for the given id",
+                code: 404
+            }
+        }
+        return response;
+    }
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+    
+}
+
 module.exports = {
     createTheatre
 }
