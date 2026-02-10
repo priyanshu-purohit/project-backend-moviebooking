185d9e95720c352bad5ca808e9afc0aeb9001beb
priyanshu-purohit <purohitpriyanshu20@gmail.com>
2026-02-10 20:27:53 +0530

    Feat: Added validations for the incoming request in reset password and update user

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -84,10 +84,27 @@ const isAuthenticated = async (req, res, next) => {
         }
         errorResponseBody.err = error;
         return res. status(500).json(errorResponseBody);
+    } 
+}
+
+const validateResetPasswordRequest = (req, res, next) => {
+    //validate old password presence
+    if(!req.body.oldPassword) {
+        errorResponseBody.err = "Missing the old password in the request";
+        return res.status(400).json(errorResponseBody);
     }
+
+    //validate new password presence
+    if(!req.body.newPassword) {
+        errorResponseBody.err = "Missing the new password in the request";
+        return res.status(400).json(errorResponseBody);
+    }
+
+    next();
 }
 module.exports = {
     validateSignupRequest,
     validateSigninRequest,
-    isAuthenticated
+    isAuthenticated,
+    validateResetPasswordRequest
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,10 @@
 const userController = require('../controller/user.controller');
+const userMiddleware = require('../middlewares/user.middlewares');
 
 const routes = (app) => {
     app.patch(
         '/mba/api/v1/user/:id',
+        userMiddleware.validateUpdateUserRequest,
         userController.update
     ); 
 
