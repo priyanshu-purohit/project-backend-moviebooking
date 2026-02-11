6c43c9ed32be351c48912a2298e1ec07d309c726
priyanshu-purohit <purohitpriyanshu20@gmail.com>
2026-02-11 19:26:15 +0530

    Fix: Fixed the hard coded status codes for auth middleware

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { errorResponseBody } = require('../utils/responsebody');
 const userService = require('../services/user.services');
-const { USER_ROLE } = require('../utils/constants');
+const { USER_ROLE, STATUS_CODES } = require('../utils/constants');
 
 /**
  * validator for user signup
@@ -14,20 +14,20 @@ const validateSignupRequest = async (req, res, next) => {
     //validate name of the user
     if(!req.body.name) {
         errorResponseBody.err = "Name of the user is not present in the request";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
     
     //validate email of the user
     if(!req.body.email) {
         errorResponseBody.err = "Email of the user not present in the request";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
 
     }
 
     //validate passowrd present of the user
     if(!req.body.password){
         errorResponseBody.err = "Password of the user not present in the request";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
 
     //request is valid
@@ -45,13 +45,13 @@ const validateSigninRequest = async (req, res, next) => {
     //validate user email presence
     if(!req.body.email) {
         errorResponseBody.err = "No email provided for sign in";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
 
     //validate user password presence
     if(!req.body.password){
         errorResponseBody.err = "No password provided for sign in";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
 
     //request is valid
@@ -63,12 +63,12 @@ const isAuthenticated = async (req, res, next) => {
         const token = req.headers["x-access-token"];
         if(!token){
             errorResponseBody.err = "No token provided";
-            return res.status(403).json(errorResponseBody);
+            return res.status(STATUS_CODES.FORBIDDEN).json(errorResponseBody);
         }
         const response = jwt.verify(token, process.env.AUTH_KEY);
         if(!response){
             errorResponseBody.err = "Token not verified";
-            return res.status(401).json(errorResponseBody);
+            return res.status(STATUS_CODES.UNAUTHORISED).json(errorResponseBody);
         }
         const user = await userService.getUserById(response.id);
         req.user = user.id;
@@ -77,14 +77,14 @@ const isAuthenticated = async (req, res, next) => {
     catch(error){
         if(error.name == "JsonWebTokenError"){
             errorResponseBody.err = error.message;
-            return res.status(401).json(errorResponseBody);
+            return res.status(STATUS_CODES.UNAUTHORISED).json(errorResponseBody);
         }
-        if(error.code == 401){
+        if(error.code == STATUS_CODES.NOT_FOUND){
             errorResponseBody.err = "User doesn't exist";
             return res.status(error.code).json(errorResponseBody);
         }
         errorResponseBody.err = error;
-        return res. status(500).json(errorResponseBody);
+        return res. status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(errorResponseBody);
     } 
 }
 
@@ -92,13 +92,13 @@ const validateResetPasswordRequest = (req, res, next) => {
     //validate old password presence
     if(!req.body.oldPassword) {
         errorResponseBody.err = "Missing the old password in the request";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
 
     //validate new password presence
     if(!req.body.newPassword) {
         errorResponseBody.err = "Missing the new password in the request";
-        return res.status(400).json(errorResponseBody);
+        return res.status(STATUS_CODES.BAD_REQUEST).json(errorResponseBody);
     }
 
     next();
@@ -108,7 +108,7 @@ const isAdmin = async (req, res, next) => {
     const user = await userService.getUserById(req.user);
     if(user.userRole != USER_ROLE.admin){
         errorResponseBody.err = "User is not admin, cannot proceed with the request";
-        return res.status(401).json(errorResponseBody);
+        return res.status(STATUS_CODES.UNAUTHORISED).json(errorResponseBody);
     }
     next();
 }
@@ -117,7 +117,7 @@ const isClient = async (req, res, next) => {
     const user = await userService.getUserById(req.user);
     if(user.userRole != USER_ROLE.customer){
         errorResponseBody.err = "User is not a client, cannot proceed with the request";
-        return res.status(401).json(errorResponseBody);
+        return res.status(STATUS_CODES.UNAUTHORISED).json(errorResponseBody);
     }
     next();
 }
@@ -126,7 +126,7 @@ const isAdminOrClient = async (req, res, next) => {
     const user = await userService.getUserById(req.user);
     if(user.userRole != USER_ROLE.customer && user.userRole != USER_ROLE.admin){
         errorResponseBody.err = "User is neither a client not an admin, cannot proceed with the request";
-        return res.status(401).json(errorResponseBody);
+        return res.status(STATUS_CODES.UNAUTHORISED).json(errorResponseBody);
     }
     next();
 }
